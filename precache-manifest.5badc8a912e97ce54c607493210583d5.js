a4c09aefafdfa475daa93889fb466ab4c681f6f4
hunfa <83254423+hunfa@users.noreply.github.com>
2022-07-27 20:58:48 +0500

    Updates

---
+++ precache-manifest.5badc8a912e97ce54c607493210583d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a456cc4d01e66c3c0f",
+    "url": "/Text-Editor/static/css/main.c9ce687c.chunk.css"
+  },
+  {
+    "revision": "a6a456cc4d01e66c3c0f",
+    "url": "/Text-Editor/static/js/main.a6a456cc.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-Editor/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-Editor/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "3898c86c0ac732aafb14",
+    "url": "/Text-Editor/static/js/runtime~main.3898c86c.js"
+  },
+  {
+    "revision": "9a62d43329950e8ad1b20d8231a4671a",
+    "url": "/Text-Editor/index.html"
+  }
+];
